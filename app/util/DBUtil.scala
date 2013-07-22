e9839d9ab616b001333acec8d12e542a1398eb4f
Faissal Boutaounte <b.faissal@gmail.com>
2013-07-22 10:35:42 -0400

    fix connection to MongoDB

--- app/util/DBUtil.scala
+++ app/util/DBUtil.scala
@@ -9,7 +9,7 @@ import com.mongodb.casbah.Imports._
  * To change this template use File | Settings | File Templates.
  */
 object DBUtil {
-        val client = MongoClient(System.getenv("OPENSHIFT_MONGODB_DB_HOST"),Integer.parseInt(System.getenv("OPENSHIFT_MONGODB_DB_PORT")))
-        val db = client("test")
+        val client = {println("=================> "+System.getenv("OPENSHIFT_MONGODB_DB_HOST")+" // "+System.getenv("OPENSHIFT_MONGODB_DB_PORT")); MongoClient(System.getenv("OPENSHIFT_MONGODB_DB_HOST"),Integer.parseInt(System.getenv("OPENSHIFT_MONGODB_DB_PORT")))}
+        val db = {val db = client("jmaghrebcfp");/*db.authenticate("admin", "nhCD2lGrPUit");*/ db }
         val speakers = db("speakers")
 }
