0665e4d4a33b1f7101d53fe0e9bc56bd6e587a54
Faissal Boutaounte <b.faissal@gmail.com>
2013-07-30 21:39:00 -0400

    fixed bugs on profile image uploader

--- target/scala-2.10/classes/public/javascripts-min/directives.js
+++ target/scala-2.10/classes/public/javascripts-min/directives.js
@@ -5,3 +5,66 @@
  * Time: 21:23
  * To change this template use File | Settings | File Templates.
  */
+angular.module('JMAGHREB', ['ngResource'])
+    .directive('uploader', function ($http) {
+        return {
+            restrict: 'E',
+            replace: true,
+            transclude: false,
+            scope: {id: '=',
+                image: '=',
+                url: "="},
+            link: function (scope, element, attrs) {
+                //attrs.id = attr.id
+                try{
+                scope.$apply(function () {
+                    scope.image = attrs.image;
+                })
+                }catch(e){
+
+                }
+
+                scope.url = attrs.url;
+                scope.id = attrs.id;
+                scope.isEdit = true;
+                //alert("Salam = "+scope.$parent.$eval(attrs.image));
+                $("#" + attrs.id).fileupload({
+                    dataType: 'json',
+                    url: attrs.url,
+                    //autoUpload: false,
+                    done: function (e, data) {
+                        $.each(data.result.files, function (index, file) {
+                            scope.$apply(function () {
+                                scope.image = file.name;
+                            })
+                        });
+                    }
+                }).on('fileuploadadd', function (e, data) {
+                        scope.isEdit = false;
+                        $.each(data.files, function (index, file) {
+
+                        });
+                    });
+                scope.canceUpload = function () {
+                    $http({method: 'GET', url: '/deleteImage/'+scope.image+'/'+scope.isEdit}).
+                        success(function(data, status, headers, config) {}).
+                        error(function(data, status, headers, config) {});
+                    scope.image = "";
+
+                }
+            },
+            template: '<div><span id="{{ id }}Btn" ng-show="image == null || image ==\'\'" class="btn btn-success fileinput-button">' +
+                '<i class="icon-plus icon-white"></i>    ' +
+                '<span>Select file ...</span>' +
+                '<!-- The file input field used as target for the file upload widget -->   ' +
+                '<input id="{{id}}" type="file" class="input-xxlarge" name="file"/> ' +
+                '</span>         ' +
+                '<div id="{{id}}Holder" ng-show="image != null && image !=\'\'" style="display: none"> ' +
+                '<div><img style="width: 150px;" ng-show="isEdit" src=\'/images/{{ image }}\'/>' +
+                '<img style="width: 150px;" ng-show="!isEdit" src=\'/tmpImages/{{ image }}\'/></div>' +
+                '<button class="btn btn-danger" ng-click="canceUpload()">Delete</button>  ' +
+                '</div>' +
+                '</div>'
+
+        }
+    })
